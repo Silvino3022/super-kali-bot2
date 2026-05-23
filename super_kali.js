fcc4ebff1735259cfe82eb8914a39ec27d1421e7
Silvino3022 <160685113+Silvino3022@users.noreply.github.com>
2026-05-23 10:08:52 -0300

    Refactor bot to use headless mode and clean code

--- super_kali.js
+++ super_kali.js
@@ -19,7 +19,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
 
     puppeteer: {
-        headless: false,
+        headless: true,
 
         args: [
             '--no-sandbox',
