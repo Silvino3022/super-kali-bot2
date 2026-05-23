d39a129793772343a386bbc300442750b025ece5
Silvino3022 <silvinofarias6@gmail.com>
2026-05-23 11:02:38 -0300

    fix railway

--- super_kali.js
+++ super_kali.js
@@ -19,8 +19,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
 
     puppeteer: {
-        headless: false,
-
+        headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
